d72e24a703bb15d33455c36a24c5d383b33d9e25
Tim Hellman <timhellman@yahoo.com>
2020-08-05 10:02:36 -0700

    adding data-time to index

--- script.js
+++ script.js
@@ -11,10 +11,36 @@
 
 
 
-
+/////FOR THE MOMENT TIMER
 var momentTimer = moment().format('MMMM Do YYYY, h:mm:ss a');
-var currentDay = document.querySelector("#currentDay")
+
+var currentDay = $("currentDay")
+$("#currentDay").append(momentTimer)
+console.log(momentTimer)
+
+/////SUBMIT THE TASKS
+////$(".btn").on("click", function(){
+    //var shoppingItem = $("#input").val();
+    //$("#list").append(`<li>${shoppingItem} <button class="purchaseButton">Purchased</button></li>`);
+//})
+//$("#list").on("click", ".purchaseButton", function(){
+    //$(this).parent("li").css("display","none")
+//})
 
 
-$(currentDay).append(momentTimer)
-console.log(currentDay)
+
+//accept user input and store it as a variable
+//add the usertext content to that variable
+//append the usertext content to class/div/id
+
+$(".saveBtn").on("click", function(){
+    var descriptionOfTask = $(".description").val();
+  
+    var time = $(this).attr("data-time")
+    console.log(descriptionOfTask);
+    localStorage.setItem(time, descriptionOfTask)
+    })
+   
+
+
+
