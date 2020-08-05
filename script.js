d9eb542cb4c2fa5eed567fc67ede2fcf16c3a940
Tim Hellman <timhellman@yahoo.com>
2020-08-04 18:37:49 -0700

    skeletal sketches

---
+++ script.js
@@ -0,0 +1,6 @@
+var momentTimer = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDay = document.querySelector("#currentDay")
+
+
+function$(currentDay).append(momentTimer)
+console.log(currentDay)
