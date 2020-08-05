171a60995f1e9c8a4d4ca682a07f280b802072d8
Tim Hellman <timhellman@yahoo.com>
2020-08-04 20:06:52 -0700

    BIG CHANGES

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+//counter with current date and timen (moment.js)
+//place counter inside the jumbotron
+//if statement - if .hour = currenttime, then present(background in description is red).  
+//else if .hour > currenttime, then future (green).    
+//else if .hour < currenttime time, then past (gray)
+
+
+
+
+
+
+
+
 var momentTimer = moment().format('MMMM Do YYYY, h:mm:ss a');
 var currentDay = document.querySelector("#currentDay")
 
 
-function$(currentDay).append(momentTimer)
+$(currentDay).append(momentTimer)
 console.log(currentDay)
