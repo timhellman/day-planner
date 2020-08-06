30288c036228c6426e6f37341006dbe19486c9ad
Tim Hellman <timhellman@yahoo.com>
2020-08-05 17:33:31 -0700

    final submission

--- script.js
+++ script.js
@@ -12,11 +12,31 @@
 
 
 /////FOR THE MOMENT TIMER
-var momentTimer = moment().format('MMMM Do YYYY, h:mm:ss a');
+var momentTimer = moment().format('MMMM Do YYYY, h:mm: A');
 
 var currentDay = $("currentDay")
 $("#currentDay").append(momentTimer)
 console.log(momentTimer)
+
+var currentHour = moment().hour()
+console.log(moment().hour())
+
+    for (var i = 0; i < 18; i++) {
+        if (i < currentHour)
+        {
+         $("#" + i).addClass("past")
+        }
+        else if (i === currentHour)
+            {
+             $("#" + i).addClass("present")
+            }
+            else 
+            {
+            $("#" + i).addClass("future")
+            }
+    }
+
+
 
 /////SUBMIT THE TASKS
 ////$(".btn").on("click", function(){
@@ -34,13 +54,28 @@ console.log(momentTimer)
 //append the usertext content to class/div/id
 
 $(".saveBtn").on("click", function(){
-    var descriptionOfTask = $(".description").val();
+    
   
     var time = $(this).attr("data-time")
+    var descriptionOfTask = $("#" + time).val();
     console.log(descriptionOfTask);
     localStorage.setItem(time, descriptionOfTask)
     })
+  
+    
+function placeTextInTaskSection() {
    
+   
+    for (var i = 0; i < 18; i++) {
+        var savedTime = localStorage.getItem(i+"")
+        if (savedTime !== null){
+        $("#"+i+"").val(savedTime)
+        }
+    }
+}    
+
+placeTextInTaskSection()
+
 
 
 
